b2b154b0fbf82d882bc9c1cc3db07d8f619a4d92
Julio-Lost <juliolost00@gmail.com>
2021-02-25 22:26:44 -0300

    add authorization in routes

---
+++ src/application/middlewares/Authorization.ts
@@ -0,0 +1,32 @@
+import { NextFunction, Request, Response } from 'express';
+import { responseTreated, unauthorized } from '@shared/useful/httpHelper';
+import { tokenDecode } from '@shared/useful/jwt';
+import unless from 'express-unless';
+
+export async function AuthorizationMiddleware(req: Request, res: Response, next: NextFunction) {
+	try {
+		const authorizationHeader = req.body.token || req.headers.authorization;
+		let message = '';
+
+		if (!authorizationHeader) {
+			message = 'Token is missing';
+			const response = unauthorized(message);
+			return responseTreated(response, res);
+		}
+		const [, token] = authorizationHeader.split(' ');
+		const userId = await tokenDecode(token);
+
+		if (userId !== null) {
+			return next();
+		}
+
+		message = 'Invalid token';
+
+		const response = unauthorized(message);
+		return responseTreated(response, res);
+	} catch (e) {
+		return next(e);
+	}
+}
+
+AuthorizationMiddleware.unless = unless;
