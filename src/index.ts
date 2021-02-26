21fee3ac98be12655fff23c31586db00d584672d
Julio-Lost <juliolost00@gmail.com>
2021-02-25 23:02:14 -0300

    test husky

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { SetupServer } from './server';
 
 (async (): Promise<void> => {
-	const port = process.env.PORT || '3333';
+	const port = process.env.PORTs || '3333';
 	const server = new SetupServer(port);
 
 	await server.init();
